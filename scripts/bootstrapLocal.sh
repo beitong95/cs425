dba45e4a6ebc84a0afa34a2c0bb2f1561eb9d99f
beitong95 <beitong2@illinois.edu>
2020-10-03 18:54:07 -0500

    add a script to clone submodules

---
+++ scripts/bootstrapLocal.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd /home/beitong2/cs425
+git submodule update --init --recursive
--- scripts/cloneRepo.sh
+++ scripts/cloneRepo.sh
@@ -4,3 +4,6 @@ rm -rf cs425/
 sleep 1
 git clone https://github.com/beitong95/cs425.git&
 sleep 5
+cd cs425
+git submodule update --init --recursive
+sleep 5
