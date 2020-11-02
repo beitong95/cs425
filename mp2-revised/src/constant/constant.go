d262eb0724d1d8a2d70e58fee32cdc4ebc6b422f
beitong95 <beitong2@illinois.edu>
2020-11-01 23:24:32 -0600

    rm mp2-revised

--- mp2-revised/src/constant/constant.go
+++
@@ -1,8 +0,0 @@
-package constant
-
-var Dir string = "/home/beitong2/cs425/mp2-revised/files/"
-var MasterHTTPServerPort = "1238" // local myport + 3 
-var DatanodeHTTPServerPort = "1239" // local myport + 1
-var DatanodeHTTPServerUploadPort = "1240" //local myport + 2 
-const MasterGetTimeout = 300 // exit3 timeout 5 mins 
-const MasterPutTimeout = 300 // exit3 timeout 5 mins 
--- mp2-revised/src/datanode/datanodeserver_test.go
+++
@@ -1,8 +0,0 @@
-package datanode_test
-import (
-	"datanode"
-	"testing"
-)
-func TestDataNode(t *testing.T){
-	datanode.ServerRun("3000")
-}
