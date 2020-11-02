54bb99c2b9c03c1971cf978d64b5750b0779dd6a
beitong95 <beitong2@illinois.edu>
2020-11-01 23:28:26 -0600

    modified bootstrap script

--- mp2/bootstrapMP2.sh
+++ mp2/bootstrapMP2.sh
@@ -2,15 +2,15 @@
 
 echo "Clean go path setting in .bashrc"
 sed -i "/\b\(GOPATH\)\b/d" ~/.bashrc
-echo "Export GOPATH setting for MP1"
-echo "export GOPATH=$HOME/cs425/mp2-revised:$HOME/cs425" >> ~/.bashrc
+echo "Export GOPATH setting for MP2"
+echo "export GOPATH=$HOME/cs425/mp2:$HOME/cs425" >> ~/.bashrc
 
 echo "Add runmp2 and debugmp2 alias"
 sed -i "/\b\(runmp2\)\b/d" ~/.bashrc
-echo "alias runmp2='cd ~/cs425/mp2-revised/src/main;go run main.go -logLevel info'" >> ~/.bashrc
+echo "alias runmp2='cd ~/cs425/mp2/src/main;go run main.go -logLevel info'" >> ~/.bashrc
 echo "runmp2 = go run main.go -logLevel info"
 sed -i "/\b\(debugmp2\)\b/d" ~/.bashrc
-echo "alias debugmp2='cd ~/cs425/mp2-revised/src/main;go run main.go -simpleCli -logLevel debug'" >> ~/.bashrc
+echo "alias debugmp2='cd ~/cs425/mp2/src/main;go run main.go -simpleCli -logLevel debug'" >> ~/.bashrc
 echo "debugmp2 = go run main.go -simpleCli -logLevel debug"
 
 echo "Add VM env variable"
@@ -18,7 +18,7 @@ sed -i "/\b\(VMNUMBER\)\b/d" ~/.bashrc
 VM=`uname -n | sed -n 's/^.*g04-\(\S*\)\.cs.*$/\1/p'`
 echo "export VMNUMBER='vm'$VM" >> ~/.bashrc
 
-echo "alias cleanRemoteFiles='rm -rf /home/beitong2/cs425/mp2-revised/files/*'" >> ~/.bashrc
+echo "alias cleanRemoteFiles='rm -rf /home/beitong2/cs425/mp2/files/*'" >> ~/.bashrc
 
 source ~/.bashrc
 
@@ -35,11 +35,11 @@ git submodule update --init --recursive
 
 
 echo "creating test files"
-dd if=/dev/urandom of=/home/beitong2/cs425/mp2-revised/src/main/1m.txt bs=1000000 count=1
-dd if=/dev/urandom of=/home/beitong2/cs425/mp2-revised/src/main/100m.txt bs=1000000 count=100
-dd if=/dev/urandom of=/home/beitong2/cs425/mp2-revised/src/main/200m.txt bs=1000000 count=200
-dd if=/dev/urandom of=/home/beitong2/cs425/mp2-revised/src/main/500m.txt bs=1000000 count=500
-dd if=/dev/urandom of=/home/beitong2/cs425/mp2-revised/src/main/1g.txt bs=1000000 count=1000
+dd if=/dev/urandom of=/home/beitong2/cs425/mp2/src/main/1m.txt bs=1000000 count=1
+dd if=/dev/urandom of=/home/beitong2/cs425/mp2/src/main/100m.txt bs=1000000 count=100
+dd if=/dev/urandom of=/home/beitong2/cs425/mp2/src/main/200m.txt bs=1000000 count=200
+dd if=/dev/urandom of=/home/beitong2/cs425/mp2/src/main/500m.txt bs=1000000 count=500
+dd if=/dev/urandom of=/home/beitong2/cs425/mp2/src/main/1g.txt bs=1000000 count=1000
 
 
 echo "Done."
